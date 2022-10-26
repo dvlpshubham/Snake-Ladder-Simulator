732e2178227414f54439d87001ba628e06122ca9
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 23:26:23 +0530

    added initial position of player 0

---
+++ com/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.snakeandladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("Initial Position of player starts from " + position);
+    }
+}
