5e8c132d6c03f1e627439fe9f476f866e718196d
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 23:47:05 +0530

    added repeat till reaches value 100

--- com/snakeandladder/SnakeAndLadder.java
+++ com/snakeandladder/SnakeAndLadder.java
@@ -9,22 +9,29 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         int position = 0;
-        Random ran = new Random();
-        int dice = ran.nextInt(6) + 1;
-        System.out.println("dice: " + dice);
+        System.out.println("Initial Position is :" + position);
 
-        int optionCheck = ran.nextInt(3);
-        System.out.println("optionCheck: " + optionCheck);
-        if (optionCheck == ladder) {
-            System.out.println("Ladder");
-            position = position + dice;
-        } else if (optionCheck == Snake && position > dice) {
-            System.out.println("Snake");
-            position = position - dice;
-        } else {
-            System.out.println("No Play");
+        while (position < 100) {
+            Random ran = new Random();
+            int dice = ran.nextInt(6) + 1;
+            System.out.println("dice: " + dice);
+
+            int optionCheck = ran.nextInt(3);
+            System.out.println("optionCheck: " + optionCheck);
+            if (optionCheck == ladder) {
+                System.out.println("Ladder");
+                position = position + dice;
+            } else if (optionCheck == Snake && position > dice) {
+                System.out.println("Snake");
+                position = position - dice;
+            } else {
+                System.out.println("No Play");
+            }
+            if (position < 0) {
+                position = 0;
+            }
+            System.out.println("position: " + position);
         }
-        System.out.println("position: " + position);
-    }
 
+    }
 }
