60b96d4f35de8149ed5e1f072eedfc6030fefe76
Shubham Raj <shubhamaryan805@gmail.com>
2022-10-26 23:39:45 +0530

    added player options Snake, Ladder and No Play

--- com/snakeandladder/SnakeAndLadder.java
+++ com/snakeandladder/SnakeAndLadder.java
@@ -3,10 +3,28 @@ package com.snakeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-    public static void main(String[] args) {
-        Random ran = new Random();
+    static final int Snake = 2;
+    static final int ladder = 1;
+    static final int Noplay = 0;
 
+    public static void main(String[] args) {
+        int position = 0;
+        Random ran = new Random();
         int dice = ran.nextInt(6) + 1;
-        System.out.println("Dice Outcome : " + dice);
+        System.out.println("dice: " + dice);
+
+        int optionCheck = ran.nextInt(3);
+        System.out.println("optionCheck: " + optionCheck);
+        if (optionCheck == ladder) {
+            System.out.println("Ladder");
+            position = position + dice;
+        } else if (optionCheck == Snake && position > dice) {
+            System.out.println("Snake");
+            position = position - dice;
+        } else {
+            System.out.println("No Play");
+        }
+        System.out.println("position: " + position);
     }
+
 }
